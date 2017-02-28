4cf1a118cf90665ff3e94aeb8943633a9f28a600
Abhijith Sreekar <nasreekar@gmail.com>
2017-02-28 11:25:18 +0800

    intitial upload

---
+++ peerjs-connection.js
@@ -0,0 +1,89 @@
+/*
+ peerjs-connection.js
+ Author: Abhijith Sreekar
+ Reference : WebRTC Fundamentals - Pluralsight.com
+ Version 1.0.0
+ -
+ This is a simple WebRTC 2-person chat, with manual signalling
+*/
+
+navigator.getWebcam = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+// PeerJS object ** FOR PRODUCTION, GET YOUR OWN KEY at http://peerjs.com/peerserver **
+var peer = new Peer({ key: '2p9ffp7ol6p3nmi',
+						debug: 3,
+						config: {'iceServers': [
+						{ url: 'stun:stun.l.google.com:19302' },
+						{ url: 'stun:stun1.l.google.com:19302' },
+						//using the same login credentials as the instructors from pluralsight
+						{ url: 'turn:numb.viagenie.ca', username:"lisa@learnfromlisa.com", credential:"22paris22"} 
+						]}});
+
+// On open, set the peer id
+peer.on('open', function(){
+	$('#my-id').text(peer.id);
+});
+
+peer.on('call', function(call) {
+	// Answer automatically for demo
+	call.answer(window.localStream);
+	step3(call);
+});
+
+// Click handlers setup
+$(function() {
+	$('#make-call').click(function() {
+		//Initiate a call!
+		var call = peer.call($('#callto-id').val(), window.localStream);
+		step3(call);
+	});
+	$('end-call').click(function() {
+		window.existingCall.close();
+		step2();
+	});
+
+	// Retry if getUserMedia fails
+	$('#step1-retry').click(function() {
+		$('#step1-error').hide();
+		step();
+	});
+
+	// Get things started
+	step1();
+});
+
+function step1() {
+	//Get audio/video stream
+	navigator.getWebcam({audio: true, video: true}, function(stream){
+		// Display the video stream in the video object
+		$('#my-video').prop('src', URL.createObjectURL(stream));
+
+		window.localStream = stream;
+		step2();
+	}, function(){ $('#step1-error').show(); });
+}
+
+function step2() { //Adjust the UI
+	$('#step1', '#step3').hide();
+	$('#step2').show();
+}
+
+function step3(call) {
+	// Hang up on an existing call if present
+	if (window.existingCall) {
+		window.existingCall.close();
+	}
+
+	// Wait for stream on the call, then setup peer video
+	call.on('stream', function(stream) {
+		$('#their-video').prop('src', URL.createObjectURL(stream));
+	});
+	$('#step1', '#step2').hide();
+	$('#step3').show();
+}
+
+
+
+
+
+
